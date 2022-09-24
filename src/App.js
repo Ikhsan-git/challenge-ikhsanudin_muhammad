9ff083d75a940f8c0faef6b309841f7723b7284a
Ikhsan-git <ikhsanudinmuhammad12@gmail.com>
2022-09-25 06:36:28 +0700

    menambahkan halaman testimoni

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import Home from './components/Home';
 import Ourservice from './components/ourservice';
 import Whyus from './components/Whyus';
+import Testimoni from './components/Testimoni';
 import './style/navbar.css';
 import './style/home.css';
 import './style/ourservice.css';
 import './style/whyus.css';
+import './style/testimoni.css';
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
       </div>
         <Ourservice />
         <Whyus />
+        <Testimoni />
       </div>
   );
 }
