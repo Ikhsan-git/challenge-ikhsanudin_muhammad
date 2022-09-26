bcf22d5966c4b9627a3d5fea3e30ab3c43559c4d
Ikhsan-git <ikhsanudinmuhammad12@gmail.com>
2022-09-27 06:33:20 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import Ourservice from './components/ourservice';
 import Whyus from './components/Whyus';
 import Testimoni from './components/Testimoni';
 import CTAbanner from './components/CTAbanner';
+import Footer from './components/Footer';
+import Fax from './components/Fax';
 import './style/navbar.css';
 import './style/home.css';
 import './style/ourservice.css';
 import './style/whyus.css';
 import './style/testimoni.css';
 import './style/CTAbanner.css';
+import './style/fax.css';
 
 function App() {
   return (
@@ -23,6 +26,8 @@ function App() {
         <Whyus />
         <Testimoni />
         <CTAbanner />
+        <Fax />
+        <Footer />
       </div>
   );
 }
