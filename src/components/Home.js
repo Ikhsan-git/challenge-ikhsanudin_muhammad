cd1ce8084a6ee7feedbf23b29b97a97df9b4ad1c
Ikhsan-git <ikhsanudinmuhammad12@gmail.com>
2022-10-01 12:04:02 +0700

    update home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,15 @@ import { Button } from "react-bootstrap"
 
 const Home = () =>{
     return(
-        <div className="hal-awal">
+        <div className="hal-awal" id="home">
             <div className='left'>
                 <div className="wrap-intro">
-                    <h1>Sewa & Rental Mobil Terbaik di kawasan (Lokasimu)</h1>
+                    <h1>Sewa 8 Rental Mobil Terbaik di kawasan (Lokasimu)</h1>
                     <p className="deskripsi-home">Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas terbaik dengan harga terjangkau. Selalu siap melayani kebutuhanmu
                         untuk sewa mobil selama 24jam.
                     </p>
-                    <Button className="bg-success btnsewa">Mulai Sewa Mobil</Button>
+                    <Button className="bg-success btnsewa" onClick= {(e) => {e.preventDefault(window.location.href='/carimobil')}}
+> Mulai Sewa Mobil </Button>
                 </div>
             </div>
             <div className='right'>
