7d1c19b1548e40cebd6d26380eeb67e14293e579
Ikhsan-git <ikhsanudinmuhammad12@gmail.com>
2022-10-01 12:04:44 +0700

    update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Whyus from './components/Whyus';
 import Testimoni from './components/Testimoni';
 import CTAbanner from './components/CTAbanner';
 import Footer from './components/Footer';
-import Fax from './components/Faq';
+import Faq from './components/Faq';
 import Carimobil from './components/Carimobil';
 import './style/navbar.css';
 import './style/home.css';
@@ -16,21 +16,30 @@ import './style/testimoni.css';
 import './style/CTAbanner.css';
 import './style/faq.css';
 import './style/footer.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <div className='home'>
+    <Router>
         <OffcanvasExample />
-        <Home />
-      </div>
-        <Ourservice />
-        <Whyus />
-        <Testimoni />
-        <CTAbanner />
-        <Fax />
+          <Switch>
+          <div className='home'>
+            <Route path="/" exact component={Home} />
+            <Route exact path="/carimobil" component={Carimobil} />
+            <Route path="/" exact component={Ourservice} />
+            <Route path="/" exact component={Whyus} />
+            <Route path="/" exact component={Testimoni} />
+            <Route path="/" exact component={CTAbanner} />
+            <Route path="/" exact component={Faq} />
+        </div>
+        </Switch>
         <Footer />
-      </div>
+    </Router>
   );
 }
 
