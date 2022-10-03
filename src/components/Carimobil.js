3839c35a0580cf7f2db4006dfc90cc751c0bb816
Ikhsan-git <ikhsanudinmuhammad12@gmail.com>
2022-10-03 16:30:01 +0700

    update page

--- src/components/Carimobil.js
+++ src/components/Carimobil.js
@@ -36,12 +36,24 @@ const Carimobil = () =>{
         </div>
     </div>
     <div className='section-form'>
-    <div className="form">
+        
+        <div className='align'>
+        <div className="form">
+        <div className='title-form'>
+          <p>Nama Mobil</p>
+          <p>Katagori</p>
+          <p>Harga</p>
+          <p>Status</p>
+          <p className='clear'></p>
+        </div>
+        <div className='form-input'>
         <input name='mobil' type="text" placeholder='Search...' className='search'/>
         <Select name='mobil'placeholder='Masukan Kapasitas Mobil' className='form-category' options={category}></Select>
         <Select name='mobil'placeholder='Masukan Harga Sewa per Hari' className='form-price' options={price}></Select>
         <Select name='mobil' placeholder='Disewa' className='form-status' options={status}></Select>
         <Button name='mobil' variant="none"className='form-button' onClick= {(e) => {e.preventDefault(window.location.href='/hasilcari')}}>Cari</Button>
+        </div>
+        </div>
         </div>
     </div>
     </div>
