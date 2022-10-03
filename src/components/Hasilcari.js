0a359f65e6dff14fdba49435cdd95728c88dfcb7
Ikhsan-git <ikhsanudinmuhammad12@gmail.com>
2022-10-03 18:25:32 +0700

    update page

--- src/components/Hasilcari.js
+++ src/components/Hasilcari.js
@@ -8,22 +8,22 @@ const Appss = () =>{
     const [query, setQuery] = useState ("");
 
     const category = [
-        { value: 'chocolate', label: 'Chocolate' },
-        { value: 'strawberry', label: 'Strawberry' },
-        { value: 'vanilla', label: 'Vanilla' }
-      ]
+      { value: '2 - 4 Orang', label: '2 - 4 Orang' },
+      { value: '4 - 6 Orang', label: '4 - 6 Orang' },
+      { value: '6 - 8 Orang', label: '6 - 8 Orang' }
+    ]
 
-      const price = [
-        { value: 'chocolate', label: 'Chocolate' },
-        { value: 'strawberry', label: 'Strawberry' },
-        { value: 'vanilla', label: 'Vanilla' }
-      ]
+    const price = [
+      { value: '< Rp. 400.000', label: '< Rp. 400.000' },
+      { value: 'Rp. 400.000 - Rp. 600.000', label: 'Rp. 400.000 - Rp. 600.000' },
+      { value: '< Rp. 400.000', label: '< Rp. 400.000' }
+    ]
 
-      const status = [
-        { value: 'chocolate', label: 'Chocolate' },
-        { value: 'strawberry', label: 'Strawberry' },
-        { value: 'vanilla', label: 'Vanilla' }
-      ]
+    const status = [
+      { value: 'Sewa', label: 'Sewa' },
+      { value: 'Disewakan', label: 'Disewakan' },
+      
+    ]
 
 
     const getCars = async () => {
@@ -58,9 +58,9 @@ const Appss = () =>{
         <div className='form-input'>
         <input name='mobil' type="text" placeholder='Search...' className='search' onChange={(e) => setQuery(e.target.value)}/>
         <Select name='mobil'placeholder='Masukan Kapasitas Mobil' className='form-category' options={category}></Select>
-        <Select name='mobil'placeholder='Masukan Harga Sewa per Hari' className='form-price' options={price}></Select>
+        <Select name='mobil'placeh older='Masukan Harga Sewa per Hari' className='form-price' options={price}></Select>
         <Select name='mobil' placeholder='Disewa' className='form-status' options={status}></Select>
-        <Button name='mobil' variant="none"className='form-button' onClick= {(e) => {e.preventDefault(window.location.href='/hasilcari')}}>Cari</Button>
+        <Button name='mobil' variant="none"className='form-button2'>Edit</Button>
         </div>
         </div>
         </div>
@@ -70,7 +70,7 @@ const Appss = () =>{
           if (!data || !data.name) {
             return false;
         }
-            
+           
             return data.name.toLowerCase().includes(query)
         }).map(data => (
             <div className='card-hasil-mobil'>
@@ -80,7 +80,7 @@ const Appss = () =>{
               <div className='category-mobil'>{data.category}</div>
               <div className='price-mobil'>Rp. {data.price} / Hari</div>
               <div className='deskripsi-mobil'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div>
-              <Button name='mobil' variant="none" className='button-mobil'>Pilih Mobil</Button>
+              <Button name='mobil-hasil' variant="none" className='button-mobil' onClick= {(e) => {e.preventDefault(window.location.href='/detailpaket')}}>Pilih Mobil</Button>
             </div> 
             </div>
           
