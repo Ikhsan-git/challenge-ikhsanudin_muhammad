9830a1a69b85be0d4131dba68a8e974dca50fd03
Ikhsan-git <ikhsanudinmuhammad12@gmail.com>
2022-10-03 18:25:52 +0700

    update page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,6 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-import { Link } from "react-router-dom";
 
 function OffcanvasExample() {
   return (
@@ -11,7 +10,7 @@ function OffcanvasExample() {
       {['lg'].map((expand) => (
         <Navbar key={expand} expand={expand} className="p-2 nav">
           <Container>
-            <Navbar.Brand className='navbar-logo'><Link to="/">BCR</Link></Navbar.Brand>
+            <Navbar.Brand className='navbar-logo' onClick= {(e) => {e.preventDefault(window.location.href='/')}}></Navbar.Brand>
 
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
 
@@ -29,10 +28,10 @@ function OffcanvasExample() {
 
               <Offcanvas.Body>
                 <Nav className=" link justify-content-end gap-4 me-5 flex-grow-1 pe-3">
-                  <Nav.Link className='text-black' href="#action1"><Link>Our Service</Link></Nav.Link>
-                  <Nav.Link className='text-black' href="#action2">Why US</Nav.Link>
-                  <Nav.Link className='text-black' href="#action2">Testimonial</Nav.Link>
-                  <Nav.Link className='text-black' href="#action2">FAQ</Nav.Link>
+                  <Nav.Link className='text-black' href="#our">Our Service</Nav.Link>
+                  <Nav.Link className='text-black' href="#whyus">Why US</Nav.Link>
+                  <Nav.Link className='text-black' href="#testimonial">Testimonial</Nav.Link>
+                  <Nav.Link className='text-black' href="#faq">FAQ</Nav.Link>
                 </Nav>
               </Offcanvas.Body>
 
